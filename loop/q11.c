972e81e8e845a084bf64bf2c54bdcc951bdbb0ac
harsh-2312 <harshlimbachiya0000@gmail.com>
2024-01-26 08:28:54 +0530

    Add folders

---
+++ loop/q11.c
@@ -0,0 +1,26 @@
+//q11
+//Accept 5 names from user at run time
+
+
+
+#include<stdio.h>
+
+int main(){
+
+ char ch[20];
+ int i;
+
+
+ printf("enter 5 name:");
+ 
+
+ for(i=1;i<=5;i++){
+     scanf("%s", &ch);
+     
+ }printf("\n");
+
+    return 0;
+
+}
+
+
---
+++ loop/q2.c
@@ -0,0 +1,21 @@
+//q2 
+//WAP to accept 5 numbers from user and display all numbers
+#include<stdio.h>
+
+int main(){
+     
+     int i=0;
+
+     printf("enter a 5 number:\n");
+     for(i=1; i<=4;i++){
+      scanf("%d ", &i);
+     
+
+     }printf("\n");
+     
+
+    return 0;
+}
+
+   
+
